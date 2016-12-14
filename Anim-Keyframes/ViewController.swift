2cbd39ea3f907c6703f8d4a1e442dcce6823f16c
Mark Jeschke <markjeschke@yahoo.com>
2016-12-14 10:00:27 -0800

    Updated individual animation layer settings.

--- Anim-Keyframes/ViewController.swift
+++ Anim-Keyframes/ViewController.swift
@@ -13,51 +13,61 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // Create JSON data extractor object for reuse
         let jsonDataExtractor = JsonDataExtractor()
         
+        // Calculated dimensions, relating to the screen's dimensions
         let shortSide = min(self.view.bounds.width, self.view.bounds.height)
         let longSide = max(self.view.bounds.width, self.view.bounds.height)
         
-        // Top Keyframe animated logo
-        let sampleLogoVector2 : KFVector!
+        ///=============== Animate Keyframe logo on the top to be played twice
         
-        // Create animation from JSON date
+        // Top Keyframe animated logo
+        let keyframeLogoVector : KFVector!
+        
+        // Create animation from JSON vector file.
         do {
-            sampleLogoVector2 = try jsonDataExtractor.loadVectorFromDisk(assetName: "sample_logo")
+            keyframeLogoVector = try jsonDataExtractor.loadVectorFromDisk(assetName: "sample_logo")
         } catch {
             print("Vector file could not be loaded, aborting")
             return
         }
         
-        let sampleLogoVectorLayer2 = KFVectorLayer()
-        sampleLogoVectorLayer2.frame = CGRect(x: shortSide / 4, y: 20, width: shortSide / 2, height: shortSide / 2)
-        sampleLogoVectorLayer2.repeatCount = 2
-        sampleLogoVectorLayer2.faceModel = sampleLogoVector2!
-        self.view.layer.addSublayer(sampleLogoVectorLayer2)
-        sampleLogoVectorLayer2.startAnimation()
+        // Create vector layer
+        let keyframeLogoVectorLayer = KFVectorLayer()
         
-        let sampleVector : KFVector!
+        keyframeLogoVectorLayer.frame = CGRect(x: shortSide / 4, y: 20, width: shortSide / 2, height: shortSide / 2)
+        
+        // The repeatCount parameter *MUST* be declared prior to the faceModel, and not after. Otherwise, the animation will not stop. If you want the animation to repeat indefinitely, just comment the following line out.
+        keyframeLogoVectorLayer.repeatCount = 2
+        
+        // Attach the animation to the faceModel.
+        keyframeLogoVectorLayer.faceModel = keyframeLogoVector!
+        
+        // Add the VectorLayer as a sublayer for the main view.
+        self.view.layer.addSublayer(keyframeLogoVectorLayer)
+        
+        // Start the animation, which will loop by default.
+        keyframeLogoVectorLayer.startAnimation()
+        
+        ///=============== Animate "S" Letter to be played once
+        
+        let sLetterVector : KFVector!
         do {
-            sampleVector = try jsonDataExtractor.loadVectorFromDisk(assetName: "keyframes")
+            sLetterVector = try jsonDataExtractor.loadVectorFromDisk(assetName: "keyframes")
         } catch {
             print("Vector file could not be loaded, aborting")
             return
         }
 
-        let sampleVectorLayer = KFVectorLayer()
-        sampleVectorLayer.frame = CGRect(x: shortSide / 4, y: longSide / 2 - shortSide / 4, width: shortSide / 2, height: shortSide / 2)
+        let sLetterVectorLayer = KFVectorLayer()
+        sLetterVectorLayer.frame = CGRect(x: shortSide / 4, y: longSide / 2 - shortSide / 4, width: shortSide / 2, height: shortSide / 2)
+        sLetterVectorLayer.repeatCount = 1
+        sLetterVectorLayer.faceModel = sLetterVector!
+        self.view.layer.addSublayer(sLetterVectorLayer)
+        sLetterVectorLayer.startAnimation()
         
-        // The repeatCount parameter *MUST* be declared prior to the faceModel, and not after. Otherwise, the animation will not stop. If you want the animation to repeat indefinitely, just comment the following line out.
-        sampleVectorLayer.repeatCount = 1
-        
-        // Attach the animation to the faceModel.
-        sampleVectorLayer.faceModel = sampleVector!
-        
-        // Add the VectorLayer as a sublayer for the main view.
-        self.view.layer.addSublayer(sampleVectorLayer)
-        
-        // Start the animation, which will loop by default.
-        sampleVectorLayer.startAnimation()
+        ///=============== Animate Keyframe logo on the bottom to be played infinitely
         
         // Bottom Keyframe animated logo
         let sampleLogoVector : KFVector!
@@ -69,6 +79,7 @@ class ViewController: UIViewController {
         }
         let sampleLogoVectorLayer = KFVectorLayer()
         sampleLogoVectorLayer.frame = CGRect(x: shortSide / 4, y: longSide - 180, width: shortSide / 2, height: shortSide / 2)
+        sLetterVectorLayer.repeatCount = 0
         sampleLogoVectorLayer.faceModel = sampleLogoVector!
         self.view.layer.addSublayer(sampleLogoVectorLayer)
         sampleLogoVectorLayer.startAnimation()
